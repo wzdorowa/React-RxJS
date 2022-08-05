3f10cb6a5fa8d3feba7ae71d6827d3d8f0118d2f
wzdorowa <w.zdorowa@gmail.com>
2022-08-05 17:05:40 +0300

    Implement adding data to localStorage.

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { TextField, Button } from "@mui/material";
 import './Form.css';
 
 function Form() {
+  const [userAddress, setUserAddress] = useState();
+  const [ERC20Address, setERC20Address] = useState();
+
+  const handleChangeUserAddress = (event) => {
+    console.log('value', event.target.value);
+    setUserAddress(event.target.value);
+  }
+
+  const handleChangeERC20Address = (event) => {
+    console.log('value', event.target.value);
+    setERC20Address(event.target.value);
+  }
+
+  const onButtonClick = () => {
+    localStorage.setItem(userAddress, ERC20Address)
+  }
+
   return (
     <div className="form">
-      <TextField id="outlined-basic" label="User address" variant="outlined" />
-      <TextField id="outlined-basic" label="ERC20 address" variant="outlined" />
-      <Button variant="contained" size="large">Send</Button>
+      <TextField onChange={handleChangeUserAddress} className="form__input" id="outlined-basic" label="User address" variant="outlined" />
+      <TextField onChange={handleChangeERC20Address} id="outlined-basic" label="ERC20 address" variant="outlined" />
+      <Button variant="contained" size="large" onClick={onButtonClick}>Watch</Button>
     </div>
   );
 }
