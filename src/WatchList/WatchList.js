7a11d294078b73fbbb6d0759301bc455c0fed679
wzdorowa <w.zdorowa@gmail.com>
2022-08-16 12:52:09 +0300

    Renamed files.

--- src/WatchList/WatchList.js
+++ src/WatchList/WatchList.js
@@ -2,8 +2,8 @@ import React, { useMemo } from 'react';
 import { CircularProgress } from '@mui/material';
 import balanceWatcher from "../BalanceWatcher";
 import useStream from '../useStream';
-import TokenBalance from '../TokenBlance/TokenBalance';
-import './List.css';
+import TokenBalance from '../TokenBalance/TokenBalance';
+import './WatchList.css';
 
 function WatchList() {
   const [watchList, error, isLoading] = useStream(useMemo(() => balanceWatcher.getWatchList(), []), []);
@@ -18,10 +18,10 @@ function WatchList() {
         <h1>Список пуст!</h1>
       } else {
         return (
-          <ul className="list">
+          <ul className="watch-list">
             {watchList.map((item) =>
-              <li key={item[0]} className="list__item">
-                <div><span className="list__item-address">{item[0]}</span> {item[1]}</div>
+              <li key={item[0]} className="watch-list__item">
+                <div><span className="watch-list__item-address">{item[0]}</span> {item[1]}</div>
                 <TokenBalance watchItem={item}/>
               </li>
             )}
